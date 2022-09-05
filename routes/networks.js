3db7d945cb1ee3445d88865d796100e43d9353df
Danby <dan.buenger@gmail.com>
2022-09-05 03:29:54 -0500

    add new tv show scraper

--- routes/networks.js
+++ routes/networks.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const Network = require('../models/network');
+const axios = require('axios').default;
+// import axios from 'axios';
+const cheerio = require('cheerio');
+// import * as cheerio from 'cheerio';
 
 const verifyToken = require('../middleware/verifyToken');
 const hasRole = require('../middleware/hasRole');
@@ -12,6 +16,52 @@ router.get('/', async (req, res) => {
     res.json(networks);
   } catch (err) {
     res.json({ message: err });
+  }
+});
+
+//Scrape new shows
+router.get('/scraper', async (req, res) => {
+  const { year } = req.body;
+  const url = `https://ew.com/tv/${year}-tv-premiere-dates/`;
+
+  let results = {
+    JAN: [],
+    FEB: [],
+    MAR: [],
+    APR: [],
+    MAY: [],
+    JUNE: [],
+    JULY: [],
+    AUG: [],
+    SEPT: [],
+    OCT: [],
+    NOV: [],
+    DEC: [],
+  };
+
+  try {
+    const html = await axios.get(url);
+    const $ = cheerio.load(html.data);
+    // get from each div with classname paragraph, "these are the release days"
+    $('div.paragraph').each((index, element) => {
+      let shows = $(element).children('p').text();
+      let regex = /(?<=SERIES PREMIERE:\s).*?\)/g;
+      let matches = shows.match(regex);
+
+      let date = $(element).find('strong').text();
+      let months = Object.keys(results);
+
+      for (let month of months) {
+        if (date.includes(month)) {
+          if (matches) {
+            results[month].push({ date, newShows: matches });
+          }
+        }
+      }
+    });
+    res.json(results);
+  } catch (err) {
+    res.json({ message: err, hmm: 'shh' });
   }
 });
 
